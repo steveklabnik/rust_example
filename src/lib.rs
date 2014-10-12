470729d1eb07c885b76bea0074a58eb8b788a88e
Steve Klabnik <steve@steveklabnik.com>
2014-10-12 14:29:53 -0400

    simpler fail_fmt
    I don't care about attaching a debugger for this simple example

--- src/lib.rs
+++ src/lib.rs
@@ -8,7 +8,7 @@ use core::str::StrSlice;
 
 #[lang = "stack_exhausted"] extern fn stack_exhausted() {}
 #[lang = "eh_personality"] extern fn eh_personality() {}
-#[lang = "fail_fmt"] fn fail_fmt() -> ! { loop {} }
+#[lang = "fail_fmt"] extern fn fail_fmt() {}
 
 #[no_mangle]
 pub extern "C" fn hello_rust() -> *const u8 {
