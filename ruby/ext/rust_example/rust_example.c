644dca12801fb5acdd934e4b638f00550ee10ab4
Steve Klabnik <steve@steveklabnik.com>
2014-10-16 09:29:16 -0400

    babby's first unsafe code :heart:

--- ruby/ext/rust_example/rust_example.c
+++ ruby/ext/rust_example/rust_example.c
@@ -2,10 +2,24 @@
 #include<ruby.h>
 
 extern char *hello_rust(void);
+extern void  fill_slice(char *);
 
 VALUE hello(void) {
     char *hello = hello_rust();
     printf("%s\n", hello);
+
+    return Qnil;
+}
+
+VALUE make_hello() {
+    // "Hello, world!".length == 14, with the null
+    char *hello = (char *)malloc(sizeof(char) * 14);
+
+    fill_slice(hello);
+
+    printf("%s\n", hello);
+
+    free(hello);
 
     return Qnil;
 }
@@ -15,4 +29,5 @@ void Init_rust_example(void) {
     VALUE rust_example = rb_define_module("RustExample");
     
     rb_define_singleton_method(rust_example, "hello", hello, 0);
+    rb_define_singleton_method(rust_example, "make_hello", make_hello, 0);
 }
--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -4,6 +4,8 @@
 #![feature(intrinsics)]
 
 extern crate core;
+extern crate rlibc;
+
 use core::str::StrSlice;
 
 #[lang = "stack_exhausted"] extern fn stack_exhausted() {}
@@ -13,6 +15,11 @@ use core::str::StrSlice;
 #[no_mangle]
 pub extern "C" fn hello_rust() -> *const u8 {
     "Hello, world!\0".as_ptr()
+}
+
+#[no_mangle]
+pub extern "C" fn fill_slice(buffer: *mut u8) {
+    unsafe { rlibc::memcpy(buffer, "Hello, world!\0".as_ptr(), 14) };
 }
 
 #[no_mangle]
